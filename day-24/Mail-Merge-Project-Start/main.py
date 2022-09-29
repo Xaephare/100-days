bb1b855f50949c8031ac9d4eca91fc18dd102fcf
Xaephare <louisgrace@onmail.com>
2022-09-29 16:52:19 +1300

    Created mail merger

--- day-24/Mail-Merge-Project-Start/main.py
+++ day-24/Mail-Merge-Project-Start/main.py
@@ -1,8 +1,11 @@
-#TODO: Create a letter using starting_letter.txt 
-#for each name in invited_names.txt
-#Replace the [name] placeholder with the actual name.
-#Save the letters in the folder "ReadyToSend".
-    
-#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+with open('Input/Names/invited_names.txt') as names:
+    content = names.read()
+    name_list = content.split('\n')
+
+with open('Input/Letters/starting_letter.txt') as letter:
+    content = letter.read()
+    for name in name_list:
+        new_letter = content.replace("[name]", name)
+        file_name = f"Output/ReadyToSend/letter_to_{name}.txt"
+        with open(file_name, 'w') as final:
+            final.write(new_letter)
