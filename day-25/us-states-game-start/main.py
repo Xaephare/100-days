40cdac380db84077011871b094e98bf229eb8ede
Xaephare <louisgrace@onmail.com>
2022-10-02 23:53:26 +1300

    changed for loop to a list comprehension

--- day-25/us-states-game-start/main.py
+++ day-25/us-states-game-start/main.py
@@ -16,13 +16,11 @@ while len(guessed_states) < 50:
                                     prompt="What's another state's name?").title()
 
     if answer_state == "Exit":
-        states_to_learn = []
-        for state in all_states:
-            if state not in guessed_states:
-                states_to_learn.append(state)
+        states_to_learn = [state for state in all_states if state not in guessed_states]
         new_data = pd.DataFrame(states_to_learn)
         new_data.to_csv("states_to_learn.csv")
         break
+
     if answer_state in all_states and answer_state not in guessed_states:
         guessed_states.append(answer_state)
         t = turtle.Turtle()
