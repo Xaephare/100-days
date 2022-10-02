8946e674d85eba94cc1f009502c41814906c8019
Xaephare <louisgrace@onmail.com>
2022-10-02 13:24:09 +1300

    added exit command added csv generation for non-guessed states guessing the same correct state to longer adds to the total correct guesses

--- day-25/us-states-game-start/main.py
+++ day-25/us-states-game-start/main.py
@@ -15,7 +15,15 @@ while len(guessed_states) < 50:
     answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
                                     prompt="What's another state's name?").title()
 
-    if answer_state in all_states:
+    if answer_state == "Exit":
+        states_to_learn = []
+        for state in all_states:
+            if state not in guessed_states:
+                states_to_learn.append(state)
+        new_data = pd.DataFrame(states_to_learn)
+        new_data.to_csv("states_to_learn.csv")
+        break
+    if answer_state in all_states and answer_state not in guessed_states:
         guessed_states.append(answer_state)
         t = turtle.Turtle()
         t.hideturtle()
