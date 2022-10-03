7e88d76c2cb9de6652f8ecab98cd1ecc60b72216
Xaephare <louisgrace@onmail.com>
2022-10-03 18:13:03 +1300

    Learning to use basic tkinter features

--- day-27/main.py
+++ day-27/main.py
@@ -1,14 +1,32 @@
-import tkinter as tk
+from tkinter import *
 
-window = tk.Tk()
+
+def button_clicked():
+    user_text = entry.get()
+    label.config(text=user_text)
+
+
+window = Tk()
 window.title("First GUI Program")
 window.minsize(width=500, height=300)
+window.config(padx=20, pady=20)
 
 # Label
+label = Label(text="I am a Label", font=('Arial', 24))
+label["text"] = "Something else"
+label.config(text="Something else")
+label.grid(column=0, row=0)
+label.config(padx=50, pady=50)
 
-label = tk.Label(text="I am a Label", font=('Arial', 24))
-label.pack()
+# Button
+button = Button(text="Click Me", command=button_clicked)
+button2 = Button(text="Second Button")
+button.grid(column=1, row=1)
+button2.grid(column=2, row=0)
 
-
+# Entry
+entry = Entry(width=20)
+print(entry.get())
+entry.grid(column=3, row=2)
 
 window.mainloop()
