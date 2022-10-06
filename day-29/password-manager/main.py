707cb770aea7a17f9c07e8093611397ca7bf79bb
Xaephare <louisgrace@onmail.com>
2022-10-06 22:10:48 +1300

    Created canvas with main logo

--- day-29/password-manager/main.py
+++ day-29/password-manager/main.py
@@ -1,5 +1,17 @@
+from tkinter import *
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Password Manager")
+window.config(padx=20, pady=20)
+
+canvas = Canvas(width=200, height=200)
+mypass_logo = PhotoImage(file='logo.png')
+canvas.create_image(100, 100, image=mypass_logo)
+canvas.grid(column=0, row=0)
+
+window.mainloop()
