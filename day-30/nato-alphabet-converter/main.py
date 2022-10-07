9a59aef26d8cd7fca6d01b2812872aaea450e1af
Xaephare <louisgrace@onmail.com>
2022-10-07 19:41:50 +1300

    Removed old commented code

--- day-30/nato-alphabet-converter/main.py
+++ day-30/nato-alphabet-converter/main.py
@@ -1,13 +1,5 @@
 import pandas
 
-#Loop through rows of a data frame
-# for (index, row) in student_data_frame.iterrows():
-#     #Access index and row
-#     #Access row.student or row.score
-#     pass
-
-# Keyword Method with iterrows()
-# {new_key:new_value for (index, row) in df.iterrows()}
 
 data = pandas.read_csv('nato_phonetic_alphabet.csv')
 nato_dict = {row.letter:row.code for (index, row) in data.iterrows()}
