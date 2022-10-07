7787039920905cde826b11a58e1103db43ae0d7c
Xaephare <louisgrace@onmail.com>
2022-10-07 19:53:21 +1300

    Added exception handling for non-alphabetical characters

--- day-30/nato-alphabet-converter/main.py
+++ day-30/nato-alphabet-converter/main.py
@@ -1,9 +1,19 @@
 import pandas
 
-
 data = pandas.read_csv('nato_phonetic_alphabet.csv')
-nato_dict = {row.letter:row.code for (index, row) in data.iterrows()}
+nato_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 
-input = input("Enter the word: ").upper()
-phonetic_output = [nato_dict[letter] for letter in input]
-print(phonetic_output)
+
+def convert_phonetic():
+    user_input = input("Enter the word: ").upper()
+
+    try:
+        phonetic_output = [nato_dict[letter] for letter in user_input]
+    except KeyError:
+        print("Sorry only letters in the alphabet please.")
+        convert_phonetic()
+    else:
+        print(phonetic_output)
+
+
+convert_phonetic()
