bf0c2ba337c46ec1fc390af1c2e4b11a3f31ec8a
Xaephare <louisgrace@onmail.com>
2022-10-17 00:25:09 +1300

    Added functionality to the tru and false buttons Added function to get next question and display it

--- day-34/quizzler-app/ui.py
+++ day-34/quizzler-app/ui.py
@@ -1,12 +1,14 @@
 from tkinter import *
-
+from quiz_brain import QuizBrain
 
 THEME_COLOR = "#375362"
 FONT = ('arial', 20, 'italic')
 
+
 class QuizInterface:
 
-    def __init__(self):
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
         self.window = Tk()
         self.window.title("Quizzler")
         self.window.config(pady=20, padx=20, bg=THEME_COLOR)
@@ -15,15 +17,30 @@ class QuizInterface:
         self.score_counter.grid(column=1, row=0)
 
         self.question_box = Canvas(width=300, height=250)
-        self.question_text = self.question_box.create_text(150, 125, text="ya mum", font=FONT, fill=THEME_COLOR)
+        self.question_text = self.question_box.create_text(150,
+                                                           125,
+                                                           text="ya mum",
+                                                           font=FONT,
+                                                           fill=THEME_COLOR,
+                                                           width=280)
         self.question_box.grid(column=0, row=1, columnspan=2, pady=50)
 
         true_img = PhotoImage(file='images/true.png')
-        self.true_button = Button(image=true_img, highlightthickness=0)
+        self.true_button = Button(image=true_img,
+                                  highlightthickness=0,
+                                  command=lambda: self.quiz.check_answer("True"))
         self.true_button.grid(column=0, row=2)
 
         false_img = PhotoImage(file='images/false.png')
-        self.false_button = Button(image=false_img, highlightthickness=0)
+        self.false_button = Button(image=false_img,
+                                   highlightthickness=0,
+                                   command=lambda: self.quiz.check_answer("False"))
         self.false_button.grid(column=1, row=2)
 
+        self.get_next_question()
+
         self.window.mainloop()
+
+    def get_next_question(self):
+        q_text = self.quiz.next_question()
+        self.question_box.itemconfig(self.question_text, text=q_text)
