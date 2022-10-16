80feb1170b57e7b374c72a5eb799c2af4f137d17
Xaephare <louisgrace@onmail.com>
2022-10-17 00:44:43 +1300

    Checks correct answer and gives color feedback to user Score is now kept

--- day-34/quizzler-app/ui.py
+++ day-34/quizzler-app/ui.py
@@ -28,13 +28,13 @@ class QuizInterface:
         true_img = PhotoImage(file='images/true.png')
         self.true_button = Button(image=true_img,
                                   highlightthickness=0,
-                                  command=lambda: self.quiz.check_answer("True"))
+                                  command=self.true_pressed)
         self.true_button.grid(column=0, row=2)
 
         false_img = PhotoImage(file='images/false.png')
         self.false_button = Button(image=false_img,
                                    highlightthickness=0,
-                                   command=lambda: self.quiz.check_answer("False"))
+                                   command=self.false_pressed)
         self.false_button.grid(column=1, row=2)
 
         self.get_next_question()
@@ -42,5 +42,25 @@ class QuizInterface:
         self.window.mainloop()
 
     def get_next_question(self):
-        q_text = self.quiz.next_question()
-        self.question_box.itemconfig(self.question_text, text=q_text)
+        self.question_box.config(bg='white')
+        if self.quiz.still_has_questions():
+            q_text = self.quiz.next_question()
+            self.question_box.itemconfig(self.question_text, text=q_text)
+        else:
+            self.question_box.itemconfig(self.question_text, text="You've reached the end of the quiz.")
+            self.true_button.config(state='disabled')
+            self.false_button.config(state='disabled')
+
+    def true_pressed(self):
+        self.give_feedback(self.quiz.check_answer("True"))
+
+    def false_pressed(self):
+        self.give_feedback(self.quiz.check_answer("False"))
+
+    def give_feedback(self, is_right):
+        if is_right:
+            self.question_box.config(bg='green')
+            self.score_counter.config(text=f"Score: {self.quiz.score}")
+        else:
+            self.question_box.config(bg='red')
+        self.window.after(1000, self.get_next_question)
