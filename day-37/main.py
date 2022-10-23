216f77ce1d7241da75fe537fcfa11d9e2cc2a6b2
Xaephare <louisgrace@onmail.com>
2022-10-24 00:19:05 +1300

    Added ability to add,update, or delete amounts of pushups on the graph

--- day-37/main.py
+++ day-37/main.py
@@ -1,12 +1,21 @@
 import requests
 import os
 from dotenv import load_dotenv
+from datetime import datetime
 
 load_dotenv()
+USERNAME = "xaephare"
+TOKEN = os.getenv("TOKEN")
+GRAPH_ID = "graph1"
+
 pixela_endpoint = "https://pixe.la/v1/users"
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+
 pixela_params = {
-    "token": os.getenv("TOKEN"),
-    "username": "xaephare",
+    "token": TOKEN,
+    "username": USERNAME,
     "agreeTermsOfService": "yes",
     "notMinor": "yes"
 }
@@ -14,3 +23,41 @@ pixela_params = {
 # response = requests.post(url=pixela_endpoint, json=pixela_params)
 # print(response.text)
 
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+
+graph_config = {
+    "id": GRAPH_ID,
+    "name": "Pushup Graph",
+    "unit": "Pushup",
+    "type": "int",
+    "color": "sora"
+}
+
+# response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+# print(response.text)
+
+pixel_edit_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
+
+today = datetime.now()
+
+pixel_data = {
+    "date": today.strftime("%Y%m%d"),
+    "quantity": "15",
+}
+
+# response = requests.post(url=pixel_edit_endpoint, json=pixel_data, headers=headers)
+# print(response.text)
+
+update_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/20221023"
+
+new_pixel_data = {
+    "quantity": "25"
+}
+
+# response = requests.put(url=update_endpoint, json=new_pixel_data, headers=headers)
+# print(response.text)
+
+delete_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/20221023"
+
+# response = requests.delete(url=delete_endpoint, headers=headers)
+# print(response.text)
