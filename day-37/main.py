f2261de47aba7ca69a55e21095e9fd057a772c60
Xaephare <louisgrace@onmail.com>
2022-10-23 23:23:52 +1300

    Created new user

--- day-37/main.py
+++ day-37/main.py
@@ -1,6 +1,16 @@
 import requests
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+pixela_endpoint = "https://pixe.la/v1/users"
+pixela_params = {
+    "token": os.getenv("TOKEN"),
+    "username": "xaephare",
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes"
+}
 
-pixela_endpoint = "https://pixe.la"
+# response = requests.post(url=pixela_endpoint, json=pixela_params)
+# print(response.text)
 
-requests.post()
