db269c51059a61245c90cea93d0ce2fe098859ac
Xaephare <louisgrace@onmail.com>
2023-02-24 16:51:10 +1300

    chore: challenge completed

--- day-45/Starting Code - 100 movies to watch start/main.py
+++ day-45/Starting Code - 100 movies to watch start/main.py
@@ -5,4 +5,14 @@ URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/m
 
 # Write your code below this line 👇
 
+response = requests.get(URL)
+movie_page = response.text
 
+soup = BeautifulSoup(movie_page, "html.parser")
+titles = [title.getText() for title in soup.find_all(name="h3", class_="title")]
+
+titles.reverse()
+
+with open("movies.txt", "w") as file:
+    for title in titles:
+        file.write(f"{title}\n")
